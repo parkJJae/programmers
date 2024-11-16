ef91b06597bab7609abab36e14a9bb017e9317b4
parkJJae <wgpark0830@naver.com>
2024-11-16 20:29:31 +0900

    Programmers PCCE 기출문제

--- src/note/WaterShortage.java
+++ src/note/WaterShortage.java
@@ -5,7 +5,7 @@ public class WaterShortage {
     public int solution(int storage, int usage, int[] change) {
         int total_usage = 0;
         for(int i=0; i<change.length; i++){
-            usage += usage * (change[i] / 100.0);
+            usage += usage * (change[i] / 100.0); // 실수형 계산이기 때문 형변환 or .0 붙여주기
             total_usage += usage;
             if(total_usage > storage){
                 return i;
